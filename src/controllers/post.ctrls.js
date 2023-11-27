3efaef21780613910a4a05d69ebd2c88bd06e30c
SandraS2611 <sssandra1980@gmail.com>
2023-11-27 02:06:59 -0300

    funcan los ids

--- src/controllers/post.ctrls.js
+++ src/controllers/post.ctrls.js
@@ -1,13 +1,11 @@
-import { posts } from "../models/post.model.js"
+import { getAllPostsOfPlaces, postPlace } from "../models/post.model.js"
 
 //!CONTROLADORES DE POST
 
 export const ctrlGetAllPosts = (req, res, next) => {
   try {
-    if (posts.length < 1) {
-      return res.sendStatus(204);
-    }
-    res.status(200).json(posts);
+  const placesPosts = getAllPostsOfPlaces()
+  res.json(placesPosts)
   } catch (error) {
     next("No hay post"); //todo para que vaya al manejador de errores antes
   }
@@ -18,7 +16,7 @@ export const ctrlCreatePost = (req, res, next) => {
   try {
     const { place, comments, image } = req.body;
   
-    posts.push({ place, comments, image });
+    postPlace({ place, comments, image });
 
     res.sendStatus(201);
   } catch (error) {
--- src/models/post.model.js
+++ src/models/post.model.js
@@ -1,10 +1,20 @@
+export const placesPosts = [
+  {
+    id: Date.now(),
+    place: "Argentina",
+    comments: "Mendoza",
+    image: "jhdfhjjdfg.com",
+  },
+];
+
+export const postPlace = ({ place, comments, image }) => {
+  if (!place) return null;
+  const newPostOfPlace = { id: Date.now(), place, comments, image };
+  placesPosts.push(newPostOfPlace);
+  return newPostOfPlace;
+};
 
 
-
-export const posts = [
-    {
-      place: "Argentina",
-      comments: "Mendoza",
-      image: "jhdfhjjdfg.com",
-    },
-  ];
+export const getAllPostsOfPlaces = () => {
+  return [...placesPosts]
+}
