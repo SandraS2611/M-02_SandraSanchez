3db7e871484371941fec48221e9f40885760435b
SandraS2611 <sssandra1980@gmail.com>
2023-12-12 03:00:50 -0300

    backend eliminada

--- Backend/validations/users.validations.js
+++
@@ -1,8 +0,0 @@
-import { body } from "express-validator"
-
-export const usersValidations = [
-    body("email").notEmpty().isEmail(),
-    body("password").notEmpty().isStrongPassword({
-        minLength : 10
-    })
-  ];
