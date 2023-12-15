01b59cf0c028224f94346286df3d13936b542745
SandraS2611 <sssandra1980@gmail.com>
2023-12-15 19:12:00 -0300

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import "./Styles/boostrap.min.css";
-import "/index.css";
-import { App } from "../../App";
+import "./index.css"; 
+import { App } from "./App";
+import { BrowserRouter } from "react-router-dom";
+
 
 const element = document.getElementById("root");
 
